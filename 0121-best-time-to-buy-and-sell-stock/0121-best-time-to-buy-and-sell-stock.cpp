091bd82958cd0daaa66c9f736e0a2e86cb1c4f92
Antesh Tiwari <74955350+anteshtiwari@users.noreply.github.com>
2023-12-01 00:34:37 +0530

    Time: 101 ms (35.88%), Space: 93.8 MB (28.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,11 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int buyAtPrice = INT_MAX;
-        int maxProfit = 0;
+        int maxProfit = 0;      
         for(int i=0; i<n; i++){
             buyAtPrice = min(buyAtPrice, prices[i]);
             maxProfit = max(maxProfit, prices[i] - buyAtPrice); 
-        }
+        }   
         return maxProfit;
     }
 };
